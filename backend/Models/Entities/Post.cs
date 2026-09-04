bd633c7a6f99eb7e39904e3958cb1019a87d809d
jakekato <loveillusion02@gmail.com>
2026-09-04 17:20:05 +0800

    add post model

---
+++ backend/Models/Entities/Post.cs
@@ -0,0 +1,33 @@
+namespace Backend.Models.Entities;
+
+// NOTE: apparently the annotations shouldn't be included here also
+// Instead its actually preferable that we really just put it in configuration
+public class Post
+{
+    public int Id { get; set; }
+
+    public string Title { get; set; } = string.Empty;
+
+    // NOTE: this is a URL friendly identifier instead of just a UUID
+    public string Slug { get; set; } = string.Empty;
+
+    public string? Summary { get; set; }
+
+    public string Body { get; set; } = string.Empty;
+
+    public bool IsDraft { get; set; } = true;
+
+    public int AuthorId { get; set; }
+
+    public Author Author { get; set; } = null!;
+
+    public DateTimeOffset CreatedAt { get; set; }
+
+    public DateTimeOffset UpdatedAt { get; set; }
+
+    public DateTimeOffset? PublishedAt { get; set; }
+
+    public int ViewCount { get; set; }
+
+    public ICollection<Media> Medias { get; set; } = new List<Media>();
+}
